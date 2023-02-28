87c31afa43afddee0e298fab38e95555846bafa9
Suman Singh <sunnysuman132@gmail.com>
2023-03-01 02:06:28 +0530

    latest

---
+++ src/App65.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+
+export default function App63() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getList();
+  }, []);
+  function getList() {
+    fetch("http://localhost:3006/contacts").then((res) => {
+      res.json().then((resp) => {
+        setData(resp);
+      });
+    });
+  }
+  function deleteUser(id) {
+    fetch(`http://localhost:3006/contacts/${id}`, {
+      method: "DELETE",
+    }).then((result) => {
+      result.json().then((resp) => {
+        console.log(resp);
+        getList();
+      });
+    });
+  }
+  return (
+    <div>
+      <center>
+        <h1>Fetch user from an api in react</h1>
+        <table border="1">
+          <tbody>
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Mobile</td>
+              <td>operation</td>
+            </tr>
+            {data.map((item, i) => (
+              <tr key={i}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.mobile}</td>
+                <td>
+                  {" "}
+                  <button onClick={() => deleteUser(item.id)}>delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </center>
+    </div>
+  );
+}
--- src/App88.js
+++ src/App88.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 export default function App88() {
-   const arr = ["suman1", "suman2", "suman3", "suman4"];
+  const arr = ["suman1", "suman2", "suman3", "suman4"];
   const [arrcopy, setArrcopy] = React.useState(arr);
-  const handleDelete=(itemIndex)=> {
+  const handleDelete = (itemIndex) => {
     let newArr = arrcopy;
     let filteredArr = newArr.filter((el, i) => {
       return i !== itemIndex;
     });
     setArrcopy(filteredArr);
-  }
+  };
   return (
     <div>
       <ul>
         {arrcopy.map((el, i) => (
           <li>
-            {el} <button onClick={()=>handleDelete(i)}>Delete</button> <br /> <br />
+            {el} <button onClick={() => handleDelete(i)}>Delete</button> <br />{" "}
+            <br />
           </li>
         ))}
       </ul>
