2dca6c8fbe8ab90628ea1949f69a7edc730a60f4
Suman Singh <sunnysuman132@gmail.com>
2023-05-22 01:39:43 +0530

    allCodeBy22May

--- src/App69.js
+++ src/App69.js
@@ -1,17 +1,17 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 export default function App69() {
-    const [count,setCount]=useState(1);
-    function update(){
-        setCount((pre1)=>{
-            console.log(pre1);
-            return count +1;
-        })
-    }
+  const [count, setCount] = useState(1);
+  function update() {
+    setCount((pre1) => {
+      console.log(pre1);
+      return count + 1;
+    });
+  }
   return (
     <div>
-        <h1>Count:{count}</h1>
-        <button onClick={update}>Update count</button>
+      <h1>Count:{count}</h1>
+      <button onClick={update}>Update count</button>
     </div>
-  )
+  );
 }
